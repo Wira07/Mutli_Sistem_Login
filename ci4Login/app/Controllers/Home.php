f72ee61b4be6b09981f355b51ba47fb68ee7c1f4
Wira07 <wiralodrasaputra07@gmail.com>
2024-03-10 08:03:21 +0700

    Update File

--- ci4Login/app/Controllers/Home.php
+++ ci4Login/app/Controllers/Home.php
@@ -7,6 +7,13 @@ class Home extends BaseController
     public function index(): string
     {
         return view('auth/login');
-
+    }
+    public function register(): string
+    {
+        return view('auth/register');
+    }
+    public function user(): string
+    {
+        return view('user/index');
     }
 }
